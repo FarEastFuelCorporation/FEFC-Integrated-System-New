52c8a1e966b6d86c89858e2304c4fa8156af4d72
jimfalar@gmail.com <jimfalar@gmail.com>
2024-09-23 16:44:45 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -45,12 +45,12 @@ const upload = multer({ storage: storage });
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+// Serve static files from the React app located outside the server directory
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 // Serve Bootstrap files from the 'node_modules' folder

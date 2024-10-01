9d973933708416338e2c52609382cc5184108952
jimfalar@gmail.com <jimfalar@gmail.com>
2024-10-01 10:53:43 +0800

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -15,11 +15,4 @@ const sequelize = new Sequelize({
   timezone: "+08:00",
 });
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
-
 module.exports = sequelize;

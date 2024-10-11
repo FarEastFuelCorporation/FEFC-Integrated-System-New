cdd939f4f00d469f46e227539118fed9757a7c8d
jimfalar@gmail.com <jimfalar@gmail.com>
2024-10-11 22:18:01 +0800

    Update Attendance.js

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -3,24 +3,30 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database.js");
 
-const Attendance = sequelize.define("Attendance", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Attendance = sequelize.define(
+  "Attendance",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    employee_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM("TIME-IN", "TIME-OUT"),
+      allowNull: false,
+    },
+    synced: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false, // Make sure to include this if you're using the synced column
+    },
   },
-  employee_id: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.ENUM("TIME-IN", "TIME-OUT"),
-    allowNull: false,
-  },
-  synced: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false, // Make sure to include this if you're using the synced column
-  },
-});
+  {
+    tableName: "Attendances", // Explicitly specify the table name
+  }
+);
 
 module.exports = Attendance;

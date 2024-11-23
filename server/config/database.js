22694b2f397fdb1a4d801376324f97fb84c8a794
jimfalar@gmail.com <jimfalar@gmail.com>
2024-11-23 09:51:51 +0800

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -14,10 +14,10 @@ const sequelize = new Sequelize({
   port: process.env.DB_PORT,
   timezone: "+08:00",
   pool: {
-    max: 10, // Maximum number of connections
+    max: 60, // Maximum number of connections
     min: 0, // Minimum number of connections
     acquire: 60000, // Maximum time in ms to acquire a connection
-    idle: 10000, // Time in ms before releasing an idle connection
+    idle: 60000, // Time in ms before releasing an idle connection
   },
 });
 

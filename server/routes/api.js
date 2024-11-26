65b8cfe8735c6645d9dd0fa7eedee8aee5ce266d
jimfalar@gmail.com <jimfalar@gmail.com>
2024-11-26 09:55:21 +0800

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -69,59 +69,59 @@ router.get("/session", (req, res) => {
   console.log("Session data:", req.session);
   if (req.session.user) {
     res.status(200).json({ user: req.session.user });
-
-    router.use("/travelOrder", travelOrderRoutes);
-    router.use("/travelOrderVerify", travelOrderVerifyRoutes);
-    router.use("/leave", leaveRoutes);
-    router.use("/overtime", overtimeRoutes);
-    router.use("/workSchedule", workScheduleRoutes);
-    router.use("/switchUser", switchUserRoutes);
-    router.use("/geoTable", geoTableRoutes);
-    router.use("/request", requestsRoutes);
-    router.use("/client", clientRoutes);
-    router.use("/transporterClient", transporterClientRoutes);
-    router.use("/document", documentRoutes);
-    router.use("/logistics", logisticsRoutes);
-    router.use("/quotationForm", quotationFormRoutes);
-    router.use("/typeOfWaste", typeOfWasteRoutes);
-    router.use("/treatmentProcess", treatmentProcessRoutes);
-    router.use("/vehicleType", vehicleTypeRoutes);
-    router.use("/vehicle", vehicleRoutes);
-    router.use("/vehicleAttachment", vehicleAttachmentRoutes);
-    router.use("/vehicleMaintenanceRequest", vehicleMaintenanceRequestRoutes);
-    router.use("/scrapType", scrapTypeRoutes);
-    router.use("/treatmentMachine", treatmentMachineRoutes);
-    router.use("/attachment", attachmentRoutes);
-    router.use("/bookedTransaction", bookedTransactionRoutes);
-    router.use("/scheduledTransaction", scheduledTransactionRoutes);
-    router.use("/dispatchedTransaction", dispatchedTransactionRoutes);
-    router.use("/receivedTransaction", receivedTransactionRoutes);
-    router.use("/sortedTransaction", sortedTransactionRoutes);
-    router.use("/warehousedTransaction", warehousedTransactionRoutes);
-    router.use("/treatedTransaction", treatedTransactionRoutes);
-    router.use("/certifiedTransaction", certifiedTransactionRoutes);
-    router.use("/plasticTransaction", plasticTransactionRoutes);
-    router.use("/billedTransaction", billedTransactionRoutes);
-    router.use("/billingApprovalTransaction", billingApprovalTransactionRoutes);
-    router.use(
-      "/billingDistributionTransaction",
-      billingDistributionTransactionRoutes
-    );
-    router.use("/collectionTransaction", collectionTransactionRoutes);
-    router.use("/hrDashboard", hrDashboardRoutes);
-    router.use("/employee", employeeRoutes);
-    router.use("/employeeRecord", employeeRecordRoutes);
-    router.use("/payroll", payrollRoutes);
-    router.use("/employeeSalary", employeeSalaryRoutes);
-    router.use("/attendanceRecord", attendanceRecordRoutes);
-    router.use("/employeeAttachment", employeeAttachmentRoutes);
-    router.use("/department", departmentRoutes);
-    router.use("/medicine", medicineRoutes);
-    router.use("/medicineLog", medicineLogRoutes);
   } else {
     res.status(401).json({ error: "Not authenticated" });
   }
 });
+
+router.use("/travelOrder", travelOrderRoutes);
+router.use("/travelOrderVerify", travelOrderVerifyRoutes);
+router.use("/leave", leaveRoutes);
+router.use("/overtime", overtimeRoutes);
+router.use("/workSchedule", workScheduleRoutes);
+router.use("/switchUser", switchUserRoutes);
+router.use("/geoTable", geoTableRoutes);
+router.use("/request", requestsRoutes);
+router.use("/client", clientRoutes);
+router.use("/transporterClient", transporterClientRoutes);
+router.use("/document", documentRoutes);
+router.use("/logistics", logisticsRoutes);
+router.use("/quotationForm", quotationFormRoutes);
+router.use("/typeOfWaste", typeOfWasteRoutes);
+router.use("/treatmentProcess", treatmentProcessRoutes);
+router.use("/vehicleType", vehicleTypeRoutes);
+router.use("/vehicle", vehicleRoutes);
+router.use("/vehicleAttachment", vehicleAttachmentRoutes);
+router.use("/vehicleMaintenanceRequest", vehicleMaintenanceRequestRoutes);
+router.use("/scrapType", scrapTypeRoutes);
+router.use("/treatmentMachine", treatmentMachineRoutes);
+router.use("/attachment", attachmentRoutes);
+router.use("/bookedTransaction", bookedTransactionRoutes);
+router.use("/scheduledTransaction", scheduledTransactionRoutes);
+router.use("/dispatchedTransaction", dispatchedTransactionRoutes);
+router.use("/receivedTransaction", receivedTransactionRoutes);
+router.use("/sortedTransaction", sortedTransactionRoutes);
+router.use("/warehousedTransaction", warehousedTransactionRoutes);
+router.use("/treatedTransaction", treatedTransactionRoutes);
+router.use("/certifiedTransaction", certifiedTransactionRoutes);
+router.use("/plasticTransaction", plasticTransactionRoutes);
+router.use("/billedTransaction", billedTransactionRoutes);
+router.use("/billingApprovalTransaction", billingApprovalTransactionRoutes);
+router.use(
+  "/billingDistributionTransaction",
+  billingDistributionTransactionRoutes
+);
+router.use("/collectionTransaction", collectionTransactionRoutes);
+router.use("/hrDashboard", hrDashboardRoutes);
+router.use("/employee", employeeRoutes);
+router.use("/employeeRecord", employeeRecordRoutes);
+router.use("/payroll", payrollRoutes);
+router.use("/employeeSalary", employeeSalaryRoutes);
+router.use("/attendanceRecord", attendanceRecordRoutes);
+router.use("/employeeAttachment", employeeAttachmentRoutes);
+router.use("/department", departmentRoutes);
+router.use("/medicine", medicineRoutes);
+router.use("/medicineLog", medicineLogRoutes);
 
 router.use(error404Controller);
 

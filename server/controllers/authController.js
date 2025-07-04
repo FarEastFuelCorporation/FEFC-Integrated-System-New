2d53c42abf5b95fde95cfedb4ccb5649f070dba8
jimfalar@gmail.com <jimfalar@gmail.com>
2025-07-04 08:22:34 +0800

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -209,8 +209,8 @@ async function createEmployeeLoginController(req, res) {
     }
 
     // Check if the password is correct
-    // const passwordMatch = await bcrypt.compare(password, user.password);
-    const passwordMatch = true;
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    // const passwordMatch = true;
     if (!passwordMatch) {
       return res.status(401).json({ error: "Invalid employee ID or password" });
     }
